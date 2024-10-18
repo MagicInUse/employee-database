9981bc7836803d40da00329b4f34d22f94f8fc71
MagicInUse <jacob.norr@live.com>
2024-10-18 07:35:01 -0600

    Completed basic server communication. Could update the SQL structuring for a better json parse with how that table display is set up. Added TODOs for the menu and prompts.

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -25,10 +25,19 @@ class Cli {
      Welcome to the Big Company CLI!`);
       // call the testPrompt method for an example server response
       // this.testPrompt();
-      this.viewAllDepartments();
-      this.viewAllRoles();
-      this.viewAllEmployees();
-      this.addDepartment();
+      // this.viewAllDepartments();
+      // this.viewAllRoles();
+      // this.viewAllEmployees();
+      // this.addDepartment();
+      // this.addRole();
+      // this.addEmployee();
+      // this.updateEmployeeRole();
+      // this.updateEmployeeManager();
+      // this.viewEmployeesByManager();
+      // this.deleteDepartment();
+      // this.deleteRoles();
+      // this.deleteEmployee();
+      this.getDepartmentBudget();
   }
 
   // define the testPrompt method
@@ -104,6 +113,11 @@ class Cli {
     console.log(separator);
   }
 
+
+  /*
+    Define basic SQL functions
+  */
+
   viewAllDepartments() {
     query.getAllDepartments().then((res) => {
       console.log("Viewing all departments...");
@@ -125,6 +139,10 @@ class Cli {
     });
   }
 
+  /*
+    Define advanced SQL functions
+  */
+
   addDepartment() {
     console.log("Beginning the process of adding a department...");
     inquirer.prompt([
@@ -140,6 +158,214 @@ class Cli {
       });
     });
   }
+
+  addRole() {
+    console.log("Beginning the process of adding a role...");
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of the new role?"
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the salary of the new role?"
+      },
+      {
+        type: "input",
+        name: "department_id",
+        message: "What is the department ID of the new role?"
+      }
+    ]).then((answers) => {
+      query.addRole(answers.title, answers.salary, answers.department_id).then((_res) => { 
+        console.log("Role added successfully!"); 
+        this.viewAllRoles();
+      });
+    });
+  }
+
+  addEmployee() {
+    console.log("Beginning the process of adding an employee...");
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the first name of the new employee?"
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is the last name of the new employee?"
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "What is the role ID of the new employee?"
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "What is the manager ID of the new employee?"
+      }
+    ]).then((answers) => {
+      if (answers.manager_id === "") {
+        answers.manager_id = null;
+      }
+      query.addEmployee(answers.first_name, answers.last_name, answers.role_id, answers.manager_id).then((_res) => { 
+        console.log("Employee added successfully!"); 
+        this.viewAllEmployees();
+      });
+    });
+  }
+
+  updateEmployeeRole() {
+    console.log("Beginning the process of updating an employee role...");
+    this.viewAllEmployees();
+    this.viewAllRoles();
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "employee_id",
+        message: "What is the ID of the employee?"
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "What is the new role ID?"
+      }
+    ]).then((answers) => {
+      query.updateEmployeeRole(answers.employee_id, answers.role_id).then((_res) => { 
+        console.log("Employee role updated successfully!"); 
+        this.viewAllEmployees();
+      });
+    });
+  }
+
+  /*
+    Define Bonus SQL functions
+  */
+
+  updateEmployeeManager() {
+    console.log("Beginning the process of updating an employee manager...");
+    this.viewAllEmployees();
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "employee_id",
+        message: "What is the ID of the employee?"
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "What is the new manager ID?"
+      }
+    ]).then((answers) => {
+      if (answers.manager_id === "") {
+        answers.manager_id = null;
+      }
+      query.updateEmployeeManager(answers.employee_id, answers.manager_id).then((_res) => { 
+        console.log("Employee manager updated successfully!"); 
+        this.viewAllEmployees();
+      });
+    });
+  }
+
+  viewEmployeesByManager() {
+    console.log("Beginning the process of viewing employees by manager...");
+    this.viewAllEmployees();
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "manager_id",
+        message: "What is the ID of the manager?"
+      }
+    ]).then((answers) => {
+      query.getEmployeesByManager(answers.manager_id).then((res) => { 
+        console.log("Viewing employees by manager...");
+        this.displayTable(res);
+      });
+    });
+  }
+
+  deleteDepartment() {
+    console.log("Beginning the process of deleting a department...");
+    this.viewAllDepartments();
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "department_id",
+        message: "What is the ID of the department?"
+      }
+    ]).then((answers) => {
+      query.deleteDepartment(answers.department_id).then((_res) => { 
+        console.log("Department deleted successfully!"); 
+        this.viewAllDepartments();
+      });
+    });
+  }
+
+  deleteRoles() {
+    //TODO: add the option to replace the role with a new role
+    console.log("Beginning the process of deleting a role...");
+    this.viewAllRoles();
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "role_id",
+        message: "What is the ID of the role?"
+      }
+    ]).then((answers) => {
+      query.deleteRole(answers.role_id).then((_res) => { 
+        console.log("Role deleted successfully!"); 
+        this.viewAllRoles();
+      });
+    });
+  }
+
+  deleteEmployee() {
+    console.log("Beginning the process of deleting an employee...");
+    this.viewAllEmployees();
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "employee_id",
+        message: "What is the ID of the employee?"
+      }
+    ]).then((answers) => {
+      query.deleteEmployee(answers.employee_id).then((_res) => { 
+        console.log("Employee deleted successfully!"); 
+        this.viewAllEmployees();
+      });
+    });
+  }
+
+  getDepartmentBudget() {
+    console.log("Beginning the process of viewing the department budget utilization...");
+    this.viewAllDepartments();
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "department_id",
+        message: "What is the ID of the department?"
+      }
+    ]).then((answers) => {
+      query.getDepartmentBudget(answers.department_id).then((res) => { 
+        console.log("Viewing the total department budget utilization...");
+        this.displayTable(res);
+      });
+    });
+  }
+
+  /*
+    Define CLI user interaction functions
+  */
+
+  // TODO: add a main menu
+  // TODO: add a sub-menu for each main menu option
+  // TODO: add a back option to each sub-menu
+  // TODO: add a quit option to the main menu
+  
 }
 
 // export the Cli class
--- src/classes/Query.ts
+++ src/classes/Query.ts
@@ -3,10 +3,10 @@ import { pool, connectToDb } from '../interfaces/connection.js';
 // call the connectToDb function to connect to the PostgreSQL database
 await connectToDb();
 
-// TODO: Write a class that contains functions for
+// TODONE: Write a class that contains functions for
 // performing specific SQL queries to the database.
 class Query {
-    // TODO: View all departments
+    // TODONE: View all departments
     async getAllDepartments() {
         // SQL query
         const query = "SELECT * FROM department";
@@ -22,7 +22,7 @@ class Query {
         }
     }
 
-    // TODO: View all roles
+    // TODONE: View all roles
     async getAllRoles() {
         // SQL query
         const query = "SELECT * FROM role";
@@ -38,7 +38,7 @@ class Query {
         }
     }
 
-    // TODO: View all employees
+    // TODONE: View all employees
     // first demo function written to get all the data from the
     // employee table in the DB_NAME (.env variable) database
     // moved function but kept note for fun easter egg
@@ -57,7 +57,7 @@ class Query {
         }
     }
 
-    // TODO: Add a department
+    // TODONE: Add a department
     async addDepartment(department: string) {
         // SQL query
         const query = "INSERT INTO department (department_name) VALUES ($1)";
@@ -73,33 +73,151 @@ class Query {
         }
     }
 
-    // TODO: Add a role
+    // TODONE: Add a role
+    async addRole(title: string, salary: number, departmentId: number) {
+        // SQL query
+        const query = "INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)";
+        // try-catch block
+        try {
+            // execute the query
+            const res = await pool.query(query, [title, salary, departmentId]);
+            // return the result
+            return res;
+        } catch (err) {
+            // return the error
+            return err;
+        }
+    }
 
+    // TODONE: Add an employee
+    async addEmployee(firstName: string, lastName: string, roleId: number, managerId: number) {
+        // SQL query
+        const query = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)";
+        // try-catch block
+        try {
+            // execute the query
+            const res = await pool.query(query, [firstName, lastName, roleId, managerId]);
+            // return the result
+            return res;
+        } catch (err) {
+            // return the error
+            return err;
+        }
+    }
 
-    // TODO: Add an employee
+    // TODONE: Update an employee role
+    async updateEmployeeRole(employeeId: number, roleId: number) {
+        // SQL query
+        const query = "UPDATE employee SET role_id = $1 WHERE id = $2";
+        // try-catch block
+        try {
+            // execute the query
+            const res = await pool.query(query, [roleId, employeeId]);
+            // return the result
+            return res;
+        } catch (err) {
+            // return the error
+            return err;
+        }
+    }
 
+    // BONUS TODONE: Update an employee manager
+    async updateEmployeeManager(employeeId: number, managerId: number) {
+        // SQL query
+        const query = "UPDATE employee SET manager_id = $1 WHERE id = $2";
+        // try-catch block
+        try {
+            // execute the query
+            const res = await pool.query(query, [managerId, employeeId]);
+            // return the result
+            return res;
+        } catch (err) {
+            // return the error
+            return err;
+        }
+    }
 
-    // TODO: Update an employee role
+    // BONUS TODONE: View employees by manager
+    async getEmployeesByManager(managerId: number) {
+        // SQL query
+        const query = "SELECT * FROM employee WHERE manager_id = $1";
+        // try-catch block
+        try {
+            // execute the query
+            const res = await pool.query(query, [managerId]);
+            // return the result
+            return res.rows;
+        } catch (err) {
+            // return the error
+            return err;
+        }
+    }
 
+    // BONUS TODONE: Delete departments
+    async deleteDepartment(departmentId: number) {
+        // SQL query
+        const query = "DELETE FROM department WHERE id = $1";
+        // try-catch block
+        try {
+            // execute the query
+            const res = await pool.query(query, [departmentId]);
+            // return the result
+            return res;
+        } catch (err) {
+            // return the error
+            return err;
+        }
+    }
 
-    // BONUS TODO: Update an employee manager
+    // BONUS TODONE: Delete roles
+    async deleteRole(roleId: number) {
+        // SQL query
+        const query = "DELETE FROM role WHERE id = $1";
+        // try-catch block
+        try {
+            // execute the query
+            const res = await pool.query(query, [roleId]);
+            // return the result
+            return res;
+        } catch (err) {
+            // return the error
+            return err;
+        }
+    }
 
+    // BONUS TODONE: Delete employees
+    async deleteEmployee(employeeId: number) {
+        // SQL query
+        const query = "DELETE FROM employee WHERE id = $1";
+        // try-catch block
+        try {
+            // execute the query
+            const res = await pool.query(query, [employeeId]);
+            // return the result
+            return res;
+        } catch (err) {
+            // return the error
+            return err;
+        }
+    }
 
-    // BONUS TODO: View employees by manager
+    // BONUS TODONE: View the total utilized budget of a department
+    async getDepartmentBudget(departmentId: number) {
+        // SQL query
+        const query = "SELECT SUM(salary) FROM role WHERE department_id = $1";
+        // try-catch block
+        try {
+            // execute the query
+            const res = await pool.query(query, [departmentId]);
+            // return the result
+            return res.rows;
+        } catch (err) {
+            // return the error
+            return err;
+        }
+    }
 
-
-    // BONUS TODO: Delete departments
-
-
-    // BONUS TODO: Delete roles
-
-
-    // BONUS TODO: Delete employees
-
-
-    // BONUS TODO: View the total utilized budget of a department
-
-
+    // TODO: Clean up the queries for better json formatting on the front end
 }
 
 //export the Query class
